b6c82a4150a69464edeeae437c1fcd3774d23979
goncy <gonzalo.pozzo4@gmail.com>
2019-07-19 17:53:25 -0300

    Update help

--- help.js
+++ help.js
@@ -33,11 +33,11 @@ describe('Navegacion', () => {
   it('La url cambia al navegar entre rutas', () => {
     cy.visit('/');
 
-    cy.url().should('contains', '/search');
+    cy.url().should('contain', '/search');
     cy.data('add-link').click();
-    cy.url().should('contains', '/add');
+    cy.url().should('contain', '/add');
     cy.data('search-link').click();
-    cy.url().should('contains', '/search');
+    cy.url().should('contain', '/search');
   });
 });
 

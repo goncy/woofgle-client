8cd7129f0d6b0fe33312bb03f8b463930155e1fb
goncy <gonzalo.pozzo4@gmail.com>
2019-07-19 17:48:09 -0300

    Update help

--- help.js
+++ help.js
@@ -33,20 +33,15 @@ describe('Navegacion', () => {
   it('La url cambia al navegar entre rutas', () => {
     cy.visit('/');
 
-    cy.url().should('include', '/search');
+    cy.url().should('contains', '/search');
     cy.data('add-link').click();
-    cy.url().should('include', '/add');
+    cy.url().should('contains', '/add');
     cy.data('search-link').click();
-    cy.url().should('include', '/search');
+    cy.url().should('contains', '/search');
   });
 });
 
 // search.test.js
-function search(criteria) {
-  criteria && cy.data('search-form', 'input').type(criteria);
-  cy.data('search-form', 'button').click();
-}
-
 describe('Busqueda', () => {
   beforeEach(() => {
     cy.server();
@@ -55,75 +50,81 @@ describe('Busqueda', () => {
   });
 
   it('Los resultados se muestran en pantalla luego de hacer una busqueda', () => {
-    cy.fixture('dogs.json').then(dogs => {
+    cy.fixture('dogs').then(dogs => {
       cy.route('GET', '/dogs**', dogs);
 
-      search();
+      cy.data('search-form').submit();
 
-      cy.data('result').should('have.lengthOf', dogs.length);
+      cy.data('result').should('have.length', dogs.length);
     });
   });
 
   it('Al buscar por nombre el request contiene el texto buscado', () => {
-    cy.route('GET', '/dogs**', []).as('request');
+    cy.route('GET', '/dogs**', []).as('search');
 
-    search('Lola');
+    cy.data('search-form', 'input').type('Lola');
+    cy.data('search-form').submit();
 
-    cy.wait('@request')
+    cy.wait('@search')
       .its('url')
       .should('contain', 'Lola');
   });
 });
 
 // add.test.js
-function fillForm(nombre, image, submit) {
-  cy.data('add-form', '.name').type(nombre);
-  cy.data('add-form', '.image').type(image);
-  submit && cy.data('add-form', 'button').click();
+function fillForm(image, name) {
+  cy.data('add-form').within(form$ => {
+    cy.get('.image').type(image);
+    cy.get('.name').type(name);
+    cy.get(form$).submit();
+  });
 }
 
 describe('Agregar', () => {
   beforeEach(() => {
     cy.visit('/add');
-
-    cy.server();
   });
 
   it('El boton de agregar esta habilitado solo cuando el formulario es valido', () => {
     cy.data('add-form', 'button').should('be.disabled');
 
-    fillForm('Mongo', '//placehold.it/256/256');
+    fillForm('//placehold.it/256x256', 'Lola');
 
-    cy.data('add-form', 'button').should('not.be.disabled');
+    cy.data('add-form', 'button').should('be.enabled');
   });
 
   it('Se muestra un mensaje de confirmacion al agregar un perrito', () => {
+    cy.server();
+
     cy.route({
       method: 'POST',
       url: '/dogs**',
       status: 200,
       response: {
-        name: 'Mongo',
-        image: '//placehold.it/256/256',
+        id: 'loli',
+        name: 'Lola',
+        image: '//placehold.it/256x256',
       },
     });
 
-    fillForm('Mongo', '//placehold.it/256/256', true);
+    fillForm('//placehold.it/256x256', 'Lola');
 
     cy.contains('Tu perrito fue agregado correctamente');
   });
 
   it('Se muestra un mensaje de error is algo falla', () => {
+    cy.server();
+
     cy.route({
       method: 'POST',
       url: '/dogs**',
       status: 500,
       response: {
-        error: 'Woof',
+        error: 'woof',
       },
     });
 
-    fillForm('Mongo', '//placehold.it/256/256', true);
+    fillForm('//placehold.it/256x256', 'Lola');
 
     cy.contains(
       'Hubo un error agregando tu perrito, intentá de nuevo mas tarde'

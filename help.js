ae041bef096c5de0a69e1bf51c25f60044ad7068
goncy <gonzalo.pozzo4@gmail.com>
2019-07-23 11:37:18 -0300

    Updates help

--- help.js
+++ help.js
@@ -71,59 +71,47 @@ describe('Busqueda', () => {
 });
 
 // add.test.js
-function fillForm(image, name) {
-  cy.data('add-form').within(form$ => {
+function fillForm(image = '//placehold.it/256x256', name = 'Sarlanguito') {
+  cy.data('add-form').within(form => {
     cy.get('.image').type(image);
     cy.get('.name').type(name);
-    cy.get(form$).submit();
+    cy.get(form).submit();
   });
 }
 
 describe('Agregar', () => {
   beforeEach(() => {
     cy.visit('/add');
+
+    cy.server();
+    cy.route('POST', '/dogs**', {});
   });
 
   it('El boton de agregar esta habilitado solo cuando el formulario es valido', () => {
     cy.data('add-form', 'button').should('be.disabled');
 
-    fillForm('//placehold.it/256x256', 'Lola');
+    fillForm();
 
     cy.data('add-form', 'button').should('be.enabled');
   });
 
   it('Se muestra un mensaje de confirmacion al agregar un perrito', () => {
-    cy.server();
-
-    cy.route({
-      method: 'POST',
-      url: '/dogs**',
-      status: 200,
-      response: {
-        id: 'loli',
-        name: 'Lola',
-        image: '//placehold.it/256x256',
-      },
-    });
-
-    fillForm('//placehold.it/256x256', 'Lola');
+    fillForm();
 
     cy.contains('Tu perrito fue agregado correctamente');
   });
 
   it('Se muestra un mensaje de error is algo falla', () => {
-    cy.server();
-
     cy.route({
-      method: 'POST',
       url: '/dogs**',
+      method: 'POST',
       status: 500,
       response: {
-        error: 'woof',
+        error: 'Woof!',
       },
     });
 
-    fillForm('//placehold.it/256x256', 'Lola');
+    fillForm();
 
     cy.contains(
       'Hubo un error agregando tu perrito, intentá de nuevo mas tarde'
